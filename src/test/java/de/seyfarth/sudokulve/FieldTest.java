2635eeee4303bcabce6ff13f5c53c129948fdac1
Barbara Seyfarth <barbara@seyfarth.de>
2015-12-29 14:06:27 +0100

    #4 Convert to project language English: Class Feld -> Field

--- src/test/java/de/seyfarth/sudokulve/FieldTest.java
+++ src/test/java/de/seyfarth/sudokulve/FieldTest.java
@@ -14,19 +14,19 @@ import de.seyfarth.sudokulve.exceptions.MehrAlsEineZifferException;
  * @author barbara
  * 
  */
-public class FeldTest {
+public class FieldTest {
 
 	/**
 	 * Test method for {@link sudokuloeser.Feld#Feld(int, int, int)}.
 	 */
 	@Test
-	public final void testFeld() {
-		// Erzeuge ein Feld, das in der 2. Spalte, 3. Zeile und Block 2 ist
-		Feld test_feld = new Feld(2, 3, 2);
+	public final void testField() {
+		// Erzeuge ein Field, das in der 2. Spalte, 3. Zeile und Block 2 ist
+		Field test_feld = new Field(2, 3, 2);
 
 		// Check content of test_feld
-		assertEquals("Zeilennummer", 2, test_feld.zeile);
-		assertEquals("Spaltennummer", 3, test_feld.spalte);
+		assertEquals("Zeilennummer", 2, test_feld.row);
+		assertEquals("Spaltennummer", 3, test_feld.column);
 		assertEquals("Blocknummer", 2, test_feld.block);
 	}
 
@@ -38,56 +38,56 @@ public class FeldTest {
 	 */
 	@Test
 	public final void testFuelleMitAllenZiffernUndLeereDann() {
-		Feld test_feld = new Feld(2, 3, 2);
+		Field test_feld = new Field(2, 3, 2);
 		boolean warEnthalten = false;
 
-		// F�lle das Feld mit den Ziffern 1, 2, 3, 4
-		test_feld.fuelleMitAllenZiffern(4);
+		// F�lle das Field mit den Ziffern 1, 2, 3, 4
+		test_feld.fillWithAllNumbers(4);
 		// Entferne eine Ziffer, die nie in der Liste war
-		warEnthalten = test_feld.entferne(7);
+		warEnthalten = test_feld.remove(7);
 		assertFalse(warEnthalten);
 		// Und wieder f�llen... es sollten immer noch nur die Ziffern 1, 2, 3, 4
 		// genau einmal enthalten
-		test_feld.fuelleMitAllenZiffern(4);
+		test_feld.fillWithAllNumbers(4);
 		// Entferne Ziffer 3
-		warEnthalten = test_feld.entferne(3);
+		warEnthalten = test_feld.remove(3);
 		assertTrue(warEnthalten);
-		// Feld hat mehr als eine Ziffer
-		assertFalse(test_feld.istLeer());
-		assertFalse(test_feld.hatGenauEineZiffer());
+		// Field hat mehr als eine Ziffer
+		assertFalse(test_feld.isEmpty());
+		assertFalse(test_feld.hasSolution());
 		// Entferne Ziffer 3, da die Ziffer schon entfernt wurde, wurde auch
 		// nichts entfernt
-		warEnthalten = test_feld.entferne(3);
+		warEnthalten = test_feld.remove(3);
 		assertFalse(warEnthalten);
 		// Entferne Ziffer 1
-		warEnthalten = test_feld.entferne(1);
+		warEnthalten = test_feld.remove(1);
 		assertTrue(warEnthalten);
-		// Feld hat mehr als eine Ziffer
-		assertFalse(test_feld.istLeer());
-		assertFalse(test_feld.hatGenauEineZiffer());
+		// Field hat mehr als eine Ziffer
+		assertFalse(test_feld.isEmpty());
+		assertFalse(test_feld.hasSolution());
 		// Entferne Ziffer 4
-		warEnthalten = test_feld.entferne(4);
+		warEnthalten = test_feld.remove(4);
 		assertTrue(warEnthalten);
-		// Feld hat mehr jetzt genau eine Ziffer, n�mlich die 2
-		assertFalse(test_feld.istLeer());
-		assertTrue(test_feld.hatGenauEineZiffer());
+		// Field hat mehr jetzt genau eine Ziffer, n�mlich die 2
+		assertFalse(test_feld.isEmpty());
+		assertTrue(test_feld.hasSolution());
 		// Entferne Ziffer 1, da die Ziffer schon entfernt wurde, wurde auch
 		// nichts entfernt
-		warEnthalten = test_feld.entferne(1);
+		warEnthalten = test_feld.remove(1);
 		assertFalse(warEnthalten);
 		// Entferne nun die letzte Ziffer, die 2
-		warEnthalten = test_feld.entferne(2);
+		warEnthalten = test_feld.remove(2);
 		assertTrue(warEnthalten);
-		// Feld ist jetzt leer
-		assertTrue(test_feld.istLeer());
-		assertFalse(test_feld.hatGenauEineZiffer());
+		// Field ist jetzt leer
+		assertTrue(test_feld.isEmpty());
+		assertFalse(test_feld.hasSolution());
 		// Entferne Ziffer 3, da die Ziffer schon entfernt wurde, wurde auch
 		// nichts entfernt
-		warEnthalten = test_feld.entferne(3);
+		warEnthalten = test_feld.remove(3);
 		assertFalse(warEnthalten);
-		// Feld ist weiterhin leer
-		assertTrue(test_feld.istLeer());
-		assertFalse(test_feld.hatGenauEineZiffer());
+		// Field ist weiterhin leer
+		assertTrue(test_feld.isEmpty());
+		assertFalse(test_feld.hasSolution());
 	}
 
 	/**
@@ -95,22 +95,22 @@ public class FeldTest {
 	 */
 	@Test
 	public final void testHoleEinzigenWert() {
-		Feld test_feld = new Feld(2, 3, 2);
+		Field test_feld = new Field(2, 3, 2);
 		boolean warEnthalten = false;
 		boolean richtigeExceptionAufgetreten = false;
 		int ziffer = 0;
 
-		// Erstmal f�llen wir das Feld mit den Ziffern 1, 2, 3, 4
-		test_feld.fuelleMitAllenZiffern(4);
+		// Erstmal f�llen wir das Field mit den Ziffern 1, 2, 3, 4
+		test_feld.fillWithAllNumbers(4);
 		// Dann entfernen wir zwei von den vier Ziffern
-		warEnthalten = test_feld.entferne(4);
+		warEnthalten = test_feld.remove(4);
 		assertTrue(warEnthalten);
-		warEnthalten = test_feld.entferne(1);
+		warEnthalten = test_feld.remove(1);
 		assertTrue(warEnthalten);
 		// Jetzt versuchen wir mal, die einzige Ziffer zu holen, obwohl
-		// eigentlich noch zwei Ziffern im Feld sind
+		// eigentlich noch zwei Ziffern im Field sind
 		try {
-			ziffer = test_feld.holeEinzigenWert();
+			ziffer = test_feld.getSolution();
 			// Da hier eine Exception auftreten muss, darf das Programm dieses
 			// assert nie ausf�hren!
 			richtigeExceptionAufgetreten = false;
@@ -123,14 +123,14 @@ public class FeldTest {
 		}
 		assertTrue(richtigeExceptionAufgetreten);
 		// Jetzt entfernen wir noch eine Ziffer, so dass nur noch eine Ziffer im
-		// Feld ist
-		warEnthalten = test_feld.entferne(2);
+		// Field ist
+		warEnthalten = test_feld.remove(2);
 		assertTrue(warEnthalten);
 		// Jetzt m�sste eigentlich noch genau eine Ziffer drin sein
-		assertTrue(test_feld.hatGenauEineZiffer());
+		assertTrue(test_feld.hasSolution());
 		// Und diese Ziffer ist eine 3
 		try {
-			ziffer = test_feld.holeEinzigenWert();
+			ziffer = test_feld.getSolution();
 			// Da hier keine Exception auftreten darf, geht es einfach weiter
 			richtigeExceptionAufgetreten = true;
 		} catch (MehrAlsEineZifferException e) {
@@ -143,12 +143,12 @@ public class FeldTest {
 		assertTrue(richtigeExceptionAufgetreten);
 		assertTrue(ziffer == 3);
 		// Jetzt entfernen wir auch noch die 3
-		test_feld.entferne(3);
-		// Das Feld muss leer sein
-		assertTrue(test_feld.istLeer());
+		test_feld.remove(3);
+		// Das Field muss leer sein
+		assertTrue(test_feld.isEmpty());
 		// Das Lesen des Wertes muss eine Exception ausloesen
 		try {
-			ziffer = test_feld.holeEinzigenWert();
+			ziffer = test_feld.getSolution();
 			// Da hier eine Exception auftreten muss, darf das Programm diese
 			// Zuweisung nie ausfuehren!
 			richtigeExceptionAufgetreten = false;
@@ -168,19 +168,19 @@ public class FeldTest {
 	 */
 	@Test
 	public final void testSetzeEinzigenWert() {
-		Feld test_feld = new Feld(2, 3, 2);
+		Field test_feld = new Field(2, 3, 2);
 		int ziffer = 0;
 		boolean richtigeExceptionAufgetreten = false;
 
-		// Als erstes ueberpruefen wir, ob das Feld leer ist
-		assertTrue(test_feld.istLeer());
+		// Als erstes ueberpruefen wir, ob das Field leer ist
+		assertTrue(test_feld.isEmpty());
 		// Wir schreiben einen Wert hinein
-		test_feld.setzeEinzigenWert(234);
+		test_feld.setSolution(234);
 		// Nun muss genau eine Ziffer/Zahl drin stehen
-		assertTrue(test_feld.hatGenauEineZiffer());
+		assertTrue(test_feld.hasSolution());
 		// Und diese Zahl muss die 234 sein
 		try {
-			ziffer = test_feld.holeEinzigenWert();
+			ziffer = test_feld.getSolution();
 			// Da hier keine Exception auftreten darf, geht es einfach weiter
 			richtigeExceptionAufgetreten = true;
 		} catch (MehrAlsEineZifferException e) {
@@ -192,18 +192,18 @@ public class FeldTest {
 		}
 		assertTrue(richtigeExceptionAufgetreten);
 		assertTrue(ziffer == 234);
-		// Jetzt fuellen wir mal das Feld mir mehr Ziffern
-		test_feld.fuelleMitAllenZiffern(17);
+		// Jetzt fuellen wir mal das Field mir mehr Ziffern
+		test_feld.fillWithAllNumbers(17);
 		// Pruefen, ob es auch wirklich mehr als eine Ziffer sind
-		assertFalse(test_feld.istLeer());
-		assertFalse(test_feld.hatGenauEineZiffer());
+		assertFalse(test_feld.isEmpty());
+		assertFalse(test_feld.hasSolution());
 		// Und setzen jetzt wieder genau eine Ziffer
-		test_feld.setzeEinzigenWert(3);
+		test_feld.setSolution(3);
 		// Nun muss genau eine Zifferdrin stehen
-		assertTrue(test_feld.hatGenauEineZiffer());
+		assertTrue(test_feld.hasSolution());
 		// Und diese Ziffer muss die 3 sein
 		try {
-			ziffer = test_feld.holeEinzigenWert();
+			ziffer = test_feld.getSolution();
 			// Da hier keine Exception auftreten darf, geht es einfach weiter
 			richtigeExceptionAufgetreten = true;
 		} catch (MehrAlsEineZifferException e) {
