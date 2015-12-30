d2e6a7b922364a2d9e6bc18b009cef1bd755859b
Barbara Seyfarth <barbara@seyfarth.de>
2015-12-30 14:49:37 +0100

    Made the compiled jar executable.

--- src/main/java/de/seyfarth/sudokulve/Sudoku.java
+++ src/main/java/de/seyfarth/sudokulve/Sudoku.java
@@ -13,41 +13,41 @@ public class Sudoku {
 
         matrix = new Matrix(3, 3);
 
-        matrix.setValue(1, 1, 4);
-        matrix.setValue(1, 9, 9);
-        matrix.setValue(2, 2, 2);
-        matrix.setValue(2, 4, 1);
-        matrix.setValue(2, 6, 8);
-        matrix.setValue(2, 7, 7);
-        matrix.setValue(2, 9, 6);
-        matrix.setValue(3, 2, 1);
-        matrix.setValue(3, 3, 5);
-        matrix.setValue(3, 4, 2);
-        matrix.setValue(3, 7, 4);
-        matrix.setValue(3, 8, 8);
-        matrix.setValue(4, 5, 1);
-        matrix.setValue(4, 7, 9);
-        matrix.setValue(5, 4, 8);
-        matrix.setValue(5, 5, 5);
-        matrix.setValue(5, 6, 7);
-        matrix.setValue(5, 8, 4);
-        matrix.setValue(6, 3, 3);
+        matrix.setValue(0, 0, 4);
+        matrix.setValue(0, 8, 9);
+        matrix.setValue(1, 1, 2);
+        matrix.setValue(1, 3, 1);
+        matrix.setValue(1, 5, 8);
+        matrix.setValue(1, 6, 7);
+        matrix.setValue(1, 8, 6);
+        matrix.setValue(2, 1, 1);
+        matrix.setValue(2, 2, 5);
+        matrix.setValue(2, 3, 2);
+        matrix.setValue(2, 6, 4);
+        matrix.setValue(2, 7, 8);
+        matrix.setValue(3, 4, 1);
+        matrix.setValue(3, 6, 9);
+        matrix.setValue(4, 3, 8);
+        matrix.setValue(4, 4, 5);
+        matrix.setValue(4, 5, 7);
+        matrix.setValue(4, 7, 4);
+        matrix.setValue(5, 2, 3);
+        matrix.setValue(5, 4, 6);
+        matrix.setValue(5, 5, 2);
+        matrix.setValue(6, 1, 3);
         matrix.setValue(6, 5, 6);
-        matrix.setValue(6, 6, 2);
-        matrix.setValue(7, 2, 3);
-        matrix.setValue(7, 6, 6);
-        matrix.setValue(7, 8, 2);
-        matrix.setValue(8, 1, 2);
-        matrix.setValue(8, 3, 6);
-        matrix.setValue(8, 4, 5);
-        matrix.setValue(8, 6, 4);
-        matrix.setValue(8, 7, 1);
-        matrix.setValue(8, 8, 3);
-        matrix.setValue(9, 1, 8);
-        matrix.setValue(9, 2, 7);
-        matrix.setValue(9, 4, 3);
-        matrix.setValue(9, 7, 6);
-        matrix.setValue(9, 9, 5);
+        matrix.setValue(6, 7, 2);
+        matrix.setValue(7, 0, 2);
+        matrix.setValue(7, 2, 6);
+        matrix.setValue(7, 3, 5);
+        matrix.setValue(7, 5, 4);
+        matrix.setValue(7, 6, 1);
+        matrix.setValue(7, 7, 3);
+        matrix.setValue(8, 0, 8);
+        matrix.setValue(8, 1, 7);
+        matrix.setValue(8, 3, 3);
+        matrix.setValue(8, 6, 6);
+        matrix.setValue(8, 8, 5);
         Solver loeser = new Solver(matrix);
         try {
             loeser.fillMatrix();
@@ -55,20 +55,16 @@ public class Sudoku {
         } catch (NoSolutionException e) {
             LOG.log(Level.SEVERE, "Für das Sudoku wurde keine Loesung gefunden.", e);
         }
-        for (int zeile = 1; zeile <= matrix.getDimension(); zeile++) {
+        for (int zeile = 0; zeile < matrix.getDimension(); zeile++) {
             StringBuilder ergebnis = new StringBuilder();
-            for (int spalte = 1; spalte <= matrix.getDimension(); spalte++) {
-                try {
-                    ergebnis.append(matrix.getValue(zeile, spalte));
+            for (int spalte = 0; spalte < matrix.getDimension(); spalte++) {
+                int solution = matrix.getValue(zeile, spalte);
+                if (solution != 0) {
+                    ergebnis.append(solution);
+                } else {
                     ergebnis.append(" ");
-                } catch (IllegalStateException e) {
-                    if (e.getMessage().equals("There is more than one possible solution.")) {
-                        LOG.log(Level.WARNING, "Es gibt mehr als eine Loesung fuer das Sudoku!", e);
-                    } else if (e.getMessage().equals("There is no possible solution.")) {
-                        LOG.log(Level.WARNING, "Es gibt keine Loesung fuer das Sudoku!", e);
-                    }
-                    return;
                 }
+                ergebnis.append(" ");
             }
             System.out.println(ergebnis.toString());
         }
